43144fc0b68cc77a80c244392ce651ac1ab24dd9
binay-shah <binay18.shah@gmail.com>
2024-03-03 17:50:42 +0545

    implemented jce

---
+++ config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.microservices.demo.config.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.requestMatchers(
+                    new AntPathRequestMatcher("/actuator/**"),
+                    new AntPathRequestMatcher("/encrypt/**"),
+                    new AntPathRequestMatcher("/decrypt/**"))
+            .permitAll()
+            .anyRequest()
+            .authenticated())
+       .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
